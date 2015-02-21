1f7b848eff51bb1c36d9e9825e800f53dbaadedc
dmzx <info@dmzx-web.net>
2015-02-21 14:41:18 +0100

    files
    files

--- event/listener.php
+++ event/listener.php
@@ -1,8 +1,12 @@
 <?php
-/**
+/*
 *
+* @package Tabscroll
+* @author dmzx (www.dmzx-web.net)
+* @copyright (c) 2015 by dmzx (www.dmzx-web.net)
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* 
 */
-
 namespace dmzx\tabscroll\event;
 
 /**
--- language/en/common.php
+++ language/en/common.php
@@ -1,6 +1,11 @@
 <?php
-/**
+/*
 *
+* @package Tabscroll
+* @author dmzx (www.dmzx-web.net)
+* @copyright (c) 2015 by dmzx (www.dmzx-web.net)
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* 
 */
 
 /**
