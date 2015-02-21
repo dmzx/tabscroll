83fb70ba508ca062a49e1f4dcf471627c2e67920
dmzx <info@dmzx-web.net>
2015-02-21 14:37:20 +0100

    files
    files

---
+++ event/listener.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+*/
+
+namespace dmzx\tabscroll\event;
+
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.user_setup' => 'load_language_on_setup',
+		);
+	}
+
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'dmzx/tabscroll',
+			'lang_set' => 'common',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+}
---
+++ language/en/common.php
@@ -0,0 +1,53 @@
+<?php
+/**
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+   'SCROLL_1'   => 'Dmzx-web.net',
+   'SCROLL_2'   => 'phpBB extensions',
+   'SCROLL_3'   => 'Dmzx-web.net',
+   'SCROLL_4'   => 'Dmzx-web.net',
+   'SCROLL_5'   => 'Dmzx-web.net',
+   'SCROLL_6'   => 'Dmzx-web.net',
+   'SCROLL_7'   => 'Dmzx-web.net',
+   'SCROLL_8'   => 'Dmzx-web.net',
+   'SCROLL_9'   => 'Dmzx-web.net',
+   'SCROLL_10'   => 'Dmzx-web.net',
+   'SCROLL_11'   => 'Dmzx-web.net',
+   'SCROLL_12'   => 'Dmzx-web.net',
+   'SCROLL_13'   => 'Dmzx-web.net',
+   'SCROLL_14'   => 'Dmzx-web.net',
+   'SCROLL_15'   => 'Dmzx-web.net',
+   'SCROLL_16'   => '...',
+   'SCROLL_COPY'   => 'Created by dmzx, www.Dmzx-web.net',
+));
